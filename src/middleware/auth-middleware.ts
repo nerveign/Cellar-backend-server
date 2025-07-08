ce052a839b65c494ddab004dd83df1d7eeffdb2a
unknown <fathur.risqo@gmail.com>
2025-07-08 17:58:03 +0700

    Add zod error

--- src/middleware/auth-middleware.ts
+++ src/middleware/auth-middleware.ts
@@ -1,12 +1,20 @@
 import { NextFunction, Request, Response } from 'express';
 import { ResponseError } from '../error/response-error';
+import jwt from 'jsonwebtoken';
 
 export const protectedRoute = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const token = req.cookies.token;
+    const token = req.cookies.jwt;
     if (!token) {
-      throw new ResponseError(401, 'Unauthorized, token unavailable');
+      throw new ResponseError(401, 'Unauthorized');
     }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!);
+
+    if (!decoded) {
+      throw new ResponseError(401, 'Unauthorized');
+    }
+
     next();
   } catch (error) {
     next(error);
--- src/middleware/error-middleware.ts
+++ src/middleware/error-middleware.ts
@@ -1,8 +1,13 @@
 import { Request, Response, NextFunction } from 'express';
 import { ResponseError } from '../error/response-error';
+import { ZodError } from 'zod';
 
 export const errorMiddleware = async (error: Error, req: Request, res: Response, next: NextFunction) => {
-  if (error instanceof ResponseError) {
+  if (error instanceof ZodError) {
+    res.status(400).json({
+      errors: `Validation error : ${JSON.stringify(error)}`,
+    });
+  } else if (error instanceof ResponseError) {
     res.status(error.status).json({
       errors: error.message,
     });
