ce26dea49fb3a716d7bcd57c0323adc760c50e8b
unknown <fathur.risqo@gmail.com>
2025-07-08 17:45:01 +0700

    Add register validation for auth

---
+++ src/validation/auth-validation.ts
@@ -0,0 +1,10 @@
+import z, { ZodType } from 'zod';
+
+export class AuthValidation {
+  static readonly REGISTER: ZodType = z.object({
+    username: z.string().min(1).max(100),
+    fullName: z.string().min(1).max(100),
+    email: z.string().min(1).email(),
+    password: z.string().min(8),
+  });
+}
