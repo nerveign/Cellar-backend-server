752945bd3ae15692dd9758639170e51d85fdf07c
Rizky Albani <75643913+rzkyalbani@users.noreply.github.com>
2025-07-09 19:16:24 +0700

    Delete dist directory

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./application/app");
-const logging_1 = require("./application/logging");
-app_1.app.listen(3000, () => {
-    logging_1.logger.info('Listening on port 3000');
-});
