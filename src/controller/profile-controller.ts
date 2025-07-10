be5a2ee12a4506435167939d41c33f2ff34d817a
unknown <fathur.risqo@gmail.com>
2025-07-10 10:56:58 +0700

    Refactor: add response variable and add data object in profile-controller

--- src/controller/profile-controller.ts
+++ src/controller/profile-controller.ts
@@ -1,22 +1,31 @@
-import { NextFunction, Response } from "express";
-import { AuthUserRequest } from "../types/user-type";
-import { User } from "../models/user-model";
-import { ResponseError } from "../error/response-error";
+import { NextFunction, Response } from 'express';
+import { AuthUserRequest } from '../types/user-type';
+import { User } from '../models/user-model';
+import { ResponseError } from '../error/response-error';
 
 export class ProfileController {
-    static async getProfile(req: AuthUserRequest, res: Response, next: NextFunction): Promise<void> {
+    static async getProfile(
+        req: AuthUserRequest,
+        res: Response,
+        next: NextFunction
+    ): Promise<void> {
         try {
             const user = await User.findById(req.userId).where('-password');
             if (!user) {
                 throw new ResponseError(404, 'User not found');
             }
 
-            res.status(200).json({
+            const response = {
                 id: user.id,
                 username: user.username,
                 fullName: user.fullName,
-                email: user.email
-            })
+                profileImg: user.profileImg,
+                email: user.email,
+            };
+
+            res.status(200).json({
+                data: response,
+            });
         } catch (err) {
             next(err);
         }
